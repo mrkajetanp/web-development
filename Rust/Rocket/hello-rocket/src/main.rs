3595c8dbf33dff568181300b995b4391db2747bb
Cajetan Puchalski <cajetan.puchalski@gmail.com>
2019-04-22 21:46:51 +0200

    [Rust] Rocket - Requests #1

--- Rust/Rocket/hello-rocket/src/main.rs
+++ Rust/Rocket/hello-rocket/src/main.rs
@@ -2,12 +2,36 @@
 
 #[macro_use] extern crate rocket;
 
-#[get("/")]
-fn index() -> &'static str {
-    "Hello, world"
+#[get("/hello/<name>/<age>/<cool>")]
+fn hello(name: String, age: u8, cool: bool) -> String {
+    if cool {
+        format!("You're a cool {} year old, {}!", age, name)
+    } else {
+        format!("{}, not very cool.", name)
+    }
+}
+
+// Serving static files (the manual way)
+#[get("/files/<file..>")]
+fn files(file: PathBuf) -> Option<NamedFile> {
+    NamedFile::open(Path::new("static/").join(file)).ok()
+}
+
+// Query strings
+#[get("/queries?wave&<name>")]
+fn queries(name: &RawStr) -> String {
+    format!("Hello, {}!", name.as_str())
+}
+
+// Optional queries
+#[get("/o/queries?wave&<name>")]
+fn optional_queries(name: Option<&RawStr>) -> String {
+    name.map(|name| format!("Hi, {}!", name))
+        .unwrap_or_else(|| "Hello".into())
 }
 
 fn main() {
-    rocket::ignite().mount("/", routes![index]).launch();
+    rocket::ignite().mount("/", routes![hello]).launch();
 }
+
 
