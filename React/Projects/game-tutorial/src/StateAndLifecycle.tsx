b9d7e375613eb61340aa22ae68d937d0b304e7b5
Cajetan Puchalski <cajetan.puchalski@gmail.com>
2019-05-15 19:41:11 +0200

    [React] Basics - State and Lifecycle

--- React/Projects/game-tutorial/src/StateAndLifecycle.tsx
+++ React/Projects/game-tutorial/src/StateAndLifecycle.tsx
@@ -2,10 +2,54 @@
 import React from 'react';
 import './App.css';
 
+class Clock extends React.Component {
+    state: { date: Date };
+    timerID: NodeJS.Timeout;
+
+    constructor(props: any) {
+        super(props);
+        this.state = { date: new Date() };
+        this.timerID = setInterval(() => {}, 0);
+    }
+
+    componentDidMount() {
+        this.timerID = setInterval(
+            () => this.tick(),
+            1000
+        );
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.timerID);
+    }
+
+    tick() {
+        // this.setState((state, props) => ({
+        //     counter: state.counter + props.increment
+        // }));
+
+        this.setState({
+            date: new Date()
+        });
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>Hello, world!</h1>
+                <h2>It is {this.state.date.toLocaleTimeString()}</h2>
+            </div>
+        );
+    }
+}
+
+let element = (
+    <Clock />
+);
 
 const StateAndLifecycle: React.FC = () => {
   return (
-      <h1>TEST</h1>
+      element
   );
 }
 
