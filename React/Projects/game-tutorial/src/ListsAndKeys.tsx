49c9d0e01302f94edb90750fa8ec63235c40bdcd
Cajetan Puchalski <cajetan.puchalski@gmail.com>
2019-05-17 23:19:52 +0200

    [React] Basics - Lists and Keys #1

---
+++ React/Projects/game-tutorial/src/ListsAndKeys.tsx
@@ -0,0 +1,16 @@
+
+import React from 'react';
+import './App.css';
+
+
+let element = (
+    <h1>TEST</h1>
+)
+
+let ListsAndKeys: React.FC = () => {
+    return (
+        element
+    );
+}
+
+export default ListsAndKeys;
--- React/Projects/game-tutorial/src/index.tsx
+++ React/Projects/game-tutorial/src/index.tsx
@@ -2,10 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 // import App from './App';
-import ConditionalRendering from './ConditionalRendering';
+import ListsAndKeys from './ListsAndKeys';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<ConditionalRendering />, document.getElementById('root'));
+ReactDOM.render(<ListsAndKeys />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
