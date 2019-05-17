3281fee2bddfedf41c0851ce99a5955da972cf73
Cajetan Puchalski <cajetan.puchalski@gmail.com>
2019-05-17 23:37:07 +0200

    [React] Basics - Lists and Keys #2

--- React/Projects/game-tutorial/src/ListsAndKeys.tsx
+++ React/Projects/game-tutorial/src/ListsAndKeys.tsx
@@ -2,9 +2,26 @@
 import React from 'react';
 import './App.css';
 
+function NumberList(props: { numbers: Array<number> }) {
+    let listItems = props.numbers.map((number: number) =>
+        <li key={number.toString()}>
+            {number}
+        </li>
+    );
+
+    return (
+        <ul>{listItems}</ul>
+    )
+}
+
+let numbers = [1, 2, 3, 4, 5];
+let doubled = numbers.map(number => number * 2);
 
 let element = (
-    <h1>TEST</h1>
+    <div>
+        <NumberList numbers={numbers} />
+        <h1>{doubled}</h1>
+    </div>
 )
 
 let ListsAndKeys: React.FC = () => {
