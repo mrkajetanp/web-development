1e9e82131afc628facdcab4903818952841bfc8f
Cajetan Puchalski <cajetan.puchalski@gmail.com>
2019-05-22 15:27:28 +0200

    [React] Tutorial - Turns and Winners

--- React/Projects/game-tutorial/src/Board.tsx
+++ React/Projects/game-tutorial/src/Board.tsx
@@ -1,20 +1,51 @@
 import React from 'react';
 import Square from './Square';
 
+function calculateWinner(squares: any) {
+    let lines = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6],
+    ];
+
+    for (let i = 0 ; i < lines.length ; i++) {
+        let [a, b, c] = lines[i];
+        if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+            return squares[a];
+        }
+    }
+
+    return null;
+}
+
 class Board extends React.Component {
-    state: {squares: Array<string | null>};
+    state: {squares: Array<string | null>, xIsNext: boolean};
 
     constructor(props: any) {
         super(props);
         this.state = {
             squares: Array(9).fill(null),
+            xIsNext: true,
         };
     }
 
     handleClick = (i: number) => {
         let squares = this.state.squares.slice();
-        squares[i] = 'X';
-        this.setState({squares: squares});
+
+        if (calculateWinner(squares) || squares[i]) {
+            return;
+        }
+
+        squares[i] = this.state.xIsNext ? 'X' : 'O';
+        this.setState({
+            squares: squares,
+            xIsNext: !this.state.xIsNext,
+        });
     }
 
     renderSquare(i: any) {
@@ -27,7 +58,13 @@ class Board extends React.Component {
     }
 
     render() {
-        let status = "Next player: X";
+        let winner = calculateWinner(this.state.squares);
+        let status;
+        if (winner) {
+            status = "Winner: " + winner;
+        } else {
+            status = "Next player: " + (this.state.xIsNext ? 'X' : 'O');
+        }
 
         return (
             <div>
