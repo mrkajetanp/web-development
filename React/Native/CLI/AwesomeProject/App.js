6268db341f0eae427fca31515270fab500364c53
Cajetan Puchalski <cajetan.puchalski@gmail.com>
2019-06-15 13:46:14 +0200

    [React Native] Basics - Handling touches

--- React/Native/CLI/AwesomeProject/App.js
+++ React/Native/CLI/AwesomeProject/App.js
@@ -1,21 +1,27 @@
 import React, { Component } from 'react';
-import { Text, View } from 'react-native';
-import FlexDimensionsBasics from './components/FlexDimensions';
+// import { Text, View } from 'react-native';
+// import PizzaTranslator from './components/HandlingInput';
+// import ButtonBasics from './components/ButtonBasics';
+import Touchables from './components/Touchables';
 
-class Greeting extends Component {
-    render() {
-        return (
-            <View style={{ alignItems: 'center' }}>
-                <Text>Hello {this.props.name}!</Text>
-            </View>
-        );
-    }
-}
+// class Greeting extends Component {
+//     propTypes = {
+//         name: String,
+//     };
+//
+//     render() {
+//         return (
+//             <View style={{ alignItems: 'center' }}>
+//                 <Text>Hello {this.props.name}!</Text>
+//             </View>
+//         );
+//     }
+// }
 
 export default class App extends Component {
-  render() {
-    return (
-        <FlexDimensionsBasics />
-    );
-  }
+    render() {
+        return (
+            <Touchables />
+        );
+    }
 }
--- React/Native/CLI/AwesomeProject/components/Blinking.js
+++ React/Native/CLI/AwesomeProject/components/Blinking.js
@@ -1,7 +1,11 @@
 import React, { Component } from 'react';
-import { AppRegistry, Text, View } from 'react-native';
+import { Text, View } from 'react-native';
 
 class Blink extends Component {
+    propTypes = {
+        text: String,
+    }
+
     state = { isShowingText: true };
 
     componentDidMount() {
@@ -24,14 +28,14 @@ class Blink extends Component {
 }
 
 export default class BlinkApp extends Component {
-  render() {
-    return (
-      <View>
-        <Blink text='I love to blink' />
-        <Blink text='Yes blinking is so great' />
-        <Blink text='Why did they ever take this out of HTML' />
-        <Blink text='Look at me look at me look at me' />
-      </View>
-    );
-  }
+    render() {
+        return (
+            <View>
+                <Blink text='I love to blink' />
+                <Blink text='Yes blinking is so great' />
+                <Blink text='Why did they ever take this out of HTML' />
+                <Blink text='Look at me look at me look at me' />
+            </View>
+        );
+    }
 }
