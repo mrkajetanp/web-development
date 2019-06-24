ec40901de2d4826b4d788d36d4c96e54c9fcc6b2
Cajetan Puchalski <cajetan.puchalski@gmail.com>
2019-06-24 16:57:50 +0200

    [RxJs] Overview

---
+++ React/Projects/rxjs-example/src/index.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
+
+import { fromEvent } from 'rxjs';
+import { throttleTime, map, scan } from 'rxjs/operators';
+
+// fromEvent(document, 'click')
+//     .pipe(scan(count => count + 1, 0))
+//     .subscribe(count => console.log(`Clicked ${count} times`));
+
+// fromEvent(document, 'click')
+//     .pipe(
+//         throttleTime(1000),
+//         scan(count => count + 1, 0)
+//     )
+//     .subscribe(count => console.log(`Clicked ${count} times`));
+
+fromEvent(document, 'click')
+    .pipe(
+        throttleTime(1000),
+        map(event => event.clientX),
+        scan((count, clientX) => count + clientX, 0)
+    )
+    .subscribe(count => console.log(count));
+
+ReactDOM.render(<App />, document.getElementById('root'));
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
