d766c53765564a8a1b5dafa159f28293fd5bef2a
Cajetan Puchalski <cajetan.puchalski@gmail.com>
2019-06-27 21:19:19 +0200

    [Yoga] Overlays

---
+++ JavaScript/Yoga/YogaExample/components/Overlays.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import { View } from 'react-native';
+
+export default class Overlays extends Component {
+    render() {
+        return (
+            <View style={{
+                flex: 1,
+                width: 500,
+                height: 500,
+            }}>
+                <View style={{
+                    flex: 1,
+                    width: 100,
+                    height: 100,
+                }}/>
+                <View style={{
+                    flex: 1,
+                    width: 100,
+                    height: 100,
+                }}/>
+                <View style={{
+                    flex: 1,
+                    width: '100%',
+                    height: '100%',
+                    position: 'absolute',
+                }}/>
+            </View>
+        );
+    }
+};
