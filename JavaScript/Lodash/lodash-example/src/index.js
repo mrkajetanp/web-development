ff16ae533b4a69e32ebc94f4fc7390504f0a915b
Cajetan Puchalski <cajetan.puchalski@gmail.com>
2019-07-01 21:15:16 +0200

    [Lodash] Functions

--- JavaScript/Lodash/lodash-example/src/index.js
+++ JavaScript/Lodash/lodash-example/src/index.js
@@ -12,6 +12,16 @@ function component() {
 
     _.forEach([3, 4], value => console.log(value));
 
+    var saves = ['profile', 'settings'];
+
+    var done = _.after(saves.length, function() {
+      console.log('done saving!');
+    });
+
+    _.forEach(saves, function(type) {
+      asyncSave({ 'type': type, 'complete': done });
+    });
+
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
     return element;
 }
