bb49fe9e271bb0e29b2e43d77ccd42838b112cf4
Cajetan Puchalski <cajetan.puchalski@gmail.com>
2019-07-05 14:16:17 +0200

    [Jest] Matchers

--- JavaScript/Jest/jest-example/src/sum.test.js
+++ JavaScript/Jest/jest-example/src/sum.test.js
@@ -3,3 +3,16 @@ const sum = require('./sum');
 test('adds 1 + 2 to equal 3', () => {
   expect(sum(1, 2)).toBe(3);
 });
+
+const shoppingList = [
+  'diapers',
+  'kleenex',
+  'trash bags',
+  'paper towels',
+  'beer',
+];
+
+test('the shopping list has beer on it', () => {
+  expect(shoppingList).toContain('beer');
+  expect(new Set(shoppingList)).toContain('beer');
+});
