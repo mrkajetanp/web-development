4699d7cdffc81bb54ff4c84418c66886891110f6
Cajetan Puchalski <cajetan.puchalski@gmail.com>
2019-07-06 22:07:21 +0200

    [Jest] Setup and Teardown

--- JavaScript/Jest/jest-example/src/sum.test.js
+++ JavaScript/Jest/jest-example/src/sum.test.js
@@ -25,3 +25,19 @@ test('the data is peanut butter', done => {
 
   fetchData(callback);
 });
+
+beforeEach(() => {
+  initializeCityDatabase();
+});
+
+afterEach(() => {
+  clearCityDatabase();
+});
+
+test('city database has Vienna', () => {
+  expect(isCity('Vienna')).toBeTruthy();
+});
+
+test('city database has San Juan', () => {
+  expect(isCity('San Juan')).toBeTruthy();
+});
