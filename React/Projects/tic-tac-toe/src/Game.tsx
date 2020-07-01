366c681d972809fcc3e46dd533281e181a472424
Kajetan Puchalski <kajetan.puchalski@tuta.io>
2020-07-01 22:53:53 +0200

    [React] Tic Tac Toe - Structure & basic state

---
+++ React/Projects/tic-tac-toe/src/Game.tsx
@@ -0,0 +1,72 @@
+import React from 'react';
+
+type SquareProps = {
+  value: number;
+};
+
+type SquareState = {
+  value: string;
+};
+
+class Square extends React.Component<SquareProps, SquareState> {
+  state: SquareState = {
+    value: '',
+  }
+
+  render() {
+    return (
+      <button className="square" onClick={() => this.setState({value: 'X'})}>
+        {this.state.value}
+      </button>
+    );
+  }
+}
+
+class Board extends React.Component {
+  renderSquare(i: number) {
+    return <Square value={i} />;
+  }
+
+  render() {
+    const status = 'Next player: X';
+
+    return (
+      <div>
+        <div className="status">{status}</div>
+        <div className="board-row">
+          {this.renderSquare(0)}
+          {this.renderSquare(1)}
+          {this.renderSquare(2)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(3)}
+          {this.renderSquare(4)}
+          {this.renderSquare(5)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(6)}
+          {this.renderSquare(7)}
+          {this.renderSquare(8)}
+        </div>
+      </div>
+    );
+  }
+}
+
+class Game extends React.Component {
+  render() {
+    return (
+      <div className="game">
+        <div className="game-board">
+          <Board />
+        </div>
+        <div className="game-info">
+          <div>{ /* stat */ }</div>
+          <ol>{ /* TODO */ }</ol>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Game;
