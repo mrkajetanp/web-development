be8b902b87554fc86c8a44204d445f0da7f9c983
Kajetan Puchalski <kajetan.puchalski@tuta.io>
2020-08-13 22:07:55 +0200

    [React] Calculator App

---
+++ React/Projects/examples/calculator/src/components/App.js
@@ -0,0 +1,27 @@
+import React from "react";
+import "./App.css";
+import ButtonPanel from "./ButtonPanel";
+import Display from "./Display";
+
+import calculate from "../logic/calculate";
+
+export default class App extends React.Component {
+  state = {
+    total: null,
+    next: null,
+    operation: null,
+  };
+
+  handleClick = (buttonName) => {
+    this.setState(calculate(this.state, buttonName));
+  };
+
+  render() {
+    return (
+      <div className="component-app">
+        <Display value={this.state.next || this.state.total || "0"} />
+        <ButtonPanel clickHandler={this.handleClick} />
+      </div>
+    );
+  }
+}
---
+++ React/Projects/examples/calculator/src/logic/calculate.js
@@ -0,0 +1,84 @@
+import Big from "big.js";
+import isNumber from "./isNumber";
+import operate from "./operate";
+
+export default function calculate(obj, buttonName) {
+  if (buttonName === "AC") {
+    return {
+      total: null,
+      next: null,
+      operation: null,
+    };
+  }
+
+  if (isNumber(buttonName)) {
+    if (buttonName === "0" && obj.next === "0") {
+      return {};
+    }
+    if (obj.operation) {
+      if (obj.next) {
+        return { next: obj.next + buttonName };
+      }
+      return { next: buttonName };
+    }
+    if (obj.next) {
+      const next = obj.next === "0" ? buttonName : obj.next + buttonName;
+      return {
+        next,
+        total: null,
+      };
+    }
+    return {
+      next: buttonName,
+      total: null,
+    };
+  }
+
+  if (buttonName === "%") {
+  }
+
+  if (buttonName === ".") {
+  }
+
+  if (buttonName === "=") {
+    if (obj.next && obj.operation) {
+      return {
+        total: operate(obj.total, obj.next, obj.operation),
+        next: null,
+        operation: null,
+      };
+    } else {
+      // '=' with no operation, nothing to do
+      return {};
+    }
+  }
+
+  if (buttonName === "+/-") {
+    if (obj.next) {
+      return { next: (-1 * parseFloat(obj.next)).toString() };
+    }
+    if (obj.total) {
+      return { total: (-1 * parseFloat(obj.total)).toString() };
+    }
+    return {};
+  }
+
+  if (obj.operation) {
+    return {
+      total: operate(obj.total, obj.next, obj.operation),
+      next: null,
+      operation: buttonName,
+    };
+  }
+
+  if (!obj.next) {
+    return { operation: buttonName };
+  }
+
+  // Save the operation and move 'next' into 'total'
+  return {
+    total: obj.next,
+    next: null,
+    operation: buttonName,
+  };
+}
